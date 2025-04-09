2c4a8a614b53e9a57e7fea3ab208a22cb3bdf939
Rachith Kad <83071449+rachithkad@users.noreply.github.com>
2025-04-10 02:16:07 +0530

    Speed Insights for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${monaSans.className} antialiased pattern`}>
         {children}
-
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
